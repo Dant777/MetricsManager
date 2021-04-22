33bef1f3070b8861c58bce5f775f5f456e4b98fb
Dant777 <47300885+Dant777@users.noreply.github.com>
2021-04-22 21:12:11 +0300

    Add project files.

---
+++ MetricsManager/Helpers/ValuesHolder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsManager
+{
+    public class ValuesHolder
+    {
+        private List<string> _values;
+        public ValuesHolder()
+        {
+            _values = new List<string>() { "1", "11", "2", "22", "3", "33" };
+        }
+
+        public List<string> Values { get => _values;  set { _values = value; } }
+    }
+}
